4b73e1f59aedf5a27d0605be65889007d748fcd2
Chuankai Yue <yuechuankai@gmail.com>
2015-11-14 15:58:06 -0500

    Load User Input Done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # EngHack-F2015
 A Chrome Extension
+
+TO-DO LIST:
+Create an Extension;
+Create a Button;
+Load User Input;
+
+Get link according to user input;
+Open the tabs when User click the button;
+Option menu;
+ship;
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,15 @@
 var btn = document.createElement("BUTTON");
   	var t = document.createTextNode("Click Me");
-		btn.setAttribute('name',name);
+		btn.setAttribute('id',"clickme");
 		//btn.setAttribute("OnClick", alert("d"));
 		var place = document.getElementById("searchform");
     btn.appendChild(t);
     place.appendChild(btn);
+
+    function workFlow(){
+        //alert('hello world');
+        var userInput = document.getElementById("lst-ib").value;
+        alert(userInput);
+    }
+
+document.getElementById('clickme').addEventListener('click', workFlow);
