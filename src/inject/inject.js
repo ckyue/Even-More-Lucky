582eff641339d55c8ca06ce92f26fd77f55267e8
TeakayAero <e.e.tk@hotmail.com>
2016-02-04 00:51:01 -0500

    append button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,27 @@
 var btn = document.createElement("BUTTON");
 var t = document.createTextNode("Even More Lucky");
 btn.setAttribute('id',"clickme");
-btn.setAttribute('align',"center");
-var place = document.getElementById("searchform");
-// var place = document.getElementsByClassName("jsb");
+btn.setAttribute('align',"right");
+//var place = document.getElementById("searchform");
+//var place = document.getElementsByClassName("jsb");
 btn.appendChild(t);
-place.appendChild(btn);
+var eleMain = document.evaluate( '//*[@id="tsf"]/div[2]/div[3]/center/input[2]' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue; //Feeling lucky
+var mainbox = document.evaluate( '//*[@id="tsf"]/div[2]/div[3]/center' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
+var resultbox = document.evaluate( '//*[@id="sfopt"]' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
+
+//-------------------------------------------------------
+if (resultbox != null) {
+	resultbox.appendChild(btn);
+}
+												//NEEDS TO BE MODIFIED BY JQUERY
+if (mainbox != null) {
+	mainbox.appendChild(btn);		//append button
+	mainbox.appendChild(eleMain);	//swap position 
+}
+//-------------------------------------------------------
+
+//place.appendChild(btn);
+//*[@id="slim_appbar"]
 // var oldLucky = document.getElementsByClassName('jsb');
 // var lucky = document.getElementsByName("I'm Feeling Lucky");
 // document.oldLucky.replaceChild(btn,lucky);
@@ -15,32 +31,32 @@ place.appendChild(btn);
     // searchControl.setResultSetSize(indicator);
 
 function workFlow(){
-  var numOfLinks = 3;
-  var counter = 0;
-  var userInput = document.getElementById("lst-ib").value;
-  var exit = 0;
+	var numOfLinks = 3;
+	var counter = 0;
+	var userInput = document.getElementById("lst-ib").value;
+	var exit = 0;
         //alert(userInput);
-  window.open("https://google.ca/search?q=" + userInput, "_self");
+	window.open("https://google.ca/search?q=" + userInput, "_self");
         // var domains = document.getElementsByClassName('r');
-  var domains = [].slice.call(document.getElementsByClassName('r'), 0).reverse();
-  for (var i = 10-numOfLinks; i < domains.length; i++) {
-          // alert(domains.length);
-    var links = domains[i].getElementsByTagName('A')
-          // var links = [].slice.call(domains[i].getElementsByTagName('A'), 0).reverse();
-          for (var j = 0; j< links.length; j++) {
-          // alert(domains.length);;
-            //console.log(links[j].attributes['href'].value);
+	var domains = [].slice.call(document.getElementsByClassName('r'), 0).reverse();
+	for (var i = 10-numOfLinks; i < domains.length; i++) {
+        // alert(domains.length);
+		var links = domains[i].getElementsByTagName('A')
+        // var links = [].slice.call(domains[i].getElementsByTagName('A'), 0).reverse();
+        for (var j = 0; j< links.length; j++) {
+			// alert(domains.length);;
+			//console.log(links[j].attributes['href'].value);
             var popup = window.open(links[j].attributes['href'].value,'_blank');
             counter = counter +1;
             if(counter == numOfLinks){
               exit = 1;
               break;
             }
-          }
-          if(exit == 1){
-            break;
-            }
-          }
+        }
+        if(exit == 1){
+			break;
+        }
+    }
 }
 
 document.getElementById('clickme').addEventListener('click', workFlow);
